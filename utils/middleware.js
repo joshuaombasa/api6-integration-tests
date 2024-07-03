6f3fc5702a8ccefb1b1271582b31d62f6354fe9c
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-03 22:19:13 +0300

    initial commit

---
+++ utils/middleware.js
@@ -0,0 +1,32 @@
+const logger = require('./logger')
+
+
+const requestLogger = (request, response, next) => {
+  logger.info('PATH:', request.path)
+  logger.info('METHOD:', request.method)
+  logger.info('BODY:', request.body)
+  logger.info('...')
+  next()
+}
+
+const unknownEndpointHandler = (request, response, next) => {
+  return response.status(404).send({ errors: [{ message: 'Unknown endpoint' }] })
+}
+
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ errors: [{ message: error.message }] })
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ errors: [{ message: error.message }] })
+  }
+
+  response.status(400).send({ errors: [{ message: 'Something wet wrong' }] })
+}
+
+
+module.exports = { requestLogger, unknownEndpointHandler, errorHandler }
