dacc14ee2b8b94ba76f6734999c8b86da99d8c08
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-07 23:49:28 +0300

    initial commit

--- controllers/properties.js
+++ controllers/properties.js
@@ -11,4 +11,52 @@ usersRouter.get('/', async (request, response, next) => {
   }
 })
 
+usersRouter.get('/:id', async (request, response, next) => {
+  try {
+    const property = await Property.findById(request.params.id)
+    if (!property) {
+      return response.status(404).send({ errors: [{ message: 'Not Found' }] })
+    }
+    response.send(property)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const { name, country, price, isAvailable } = request.body
+  const propertyObject = new Property({ name, country, price, isAvailable })
+  try {
+    const savedProperty = await propertyObject.save()
+    response.status(201).send(savedProperty)
+    response.send(properties)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.put('/:id', async (request, response, next) => {
+  const { name, country, price, isAvailable } = request.body
+  try {
+    const updatedProperty = await Property.findByIdAndUpdate(
+      request.params.id,
+      { name, country, price, isAvailable },
+      { new: true }
+    )
+
+    response.send(updatedProperty)
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Property.findByIdAndDelete(request.params.id)
+    response.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = propertiesRouter
