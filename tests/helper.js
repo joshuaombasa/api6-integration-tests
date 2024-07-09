5b32e2f9ae4b69c8addef6b9f0f2af733c65fef9
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-09 23:36:22 +0300

    initial commit

---
+++ tests/helper.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+const Property = require('../models/property')
+const app = require('./app')
+const supertest = require('supertest')
+
+const api = supertest(app)
