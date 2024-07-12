6041c63600af43c6ef1075254dffc439fae2d4e7
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-12 23:41:55 +0300

    initial commit

--- tests/api6-integration-tests.test.js
+++ tests/api6-integration-tests.test.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose')
 const Property = require('../models/property')
+const app = require('./app')
+const supertest = require('supertest')
+const helper = require('./helper')
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Property.deleteMany({})
+  for (let property of helper.propertyData) {
+    const propertyObject = new Property(property)
+    await propertyObject.save()
+  }
+})
+
+
+afteAll(async () => {
+  await mongoose.connection.close()
+})
