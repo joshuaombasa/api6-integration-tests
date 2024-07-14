7d7320492bd859be47b385341414fcd680eaf4e5
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-14 11:04:47 +0300

    add integration tests

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
+
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const usersRouter = require('./controllers/users')
-const propertiesRouter = require('./controllers/properties')
 
-const cors = require('cors')
-
-const app = express()
-
-
-mongoose.connect(config.MONGO_URI)
-  .then(() => logger.info('Connected to MONGODB'))
-  .catch(error => logger.error(error))
-
-
-app.use(express.json())
-app.use(cors())
-
-app.use(middleware.requestLogger)
-
-app.use('/api/users', usersRouter)
-app.use('/api/properties', propertiesRouter)
-
-app.use(middleware.unknownEndpointHandler)
-app.use(middleware.errorHandler)
-
-
-module.exports = app
+app.listen(config.PORT, () => {
+  logger.info(`server running on port ${config.PORT}`)
+})
