f3e4c0110afd604aed146160f68cd94f4b6d82e3
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-07-14 11:05:05 +0300

    add integration tests

--- tests/helper.js
+++ tests/helper.js
@@ -2,9 +2,39 @@ const Property = require('../models/property')
 
 
 
-const propertyData = [{
+const propertyData = [
+  {
+    "name": "Sunset Villa",
+    "country": "Spain",
+    "price": 450000,
+    "isAvailable": true
+  },
+  {
+    "name": "Mountain Retreat",
+    "country": "Switzerland",
+    "price": 1200000,
+    "isAvailable": false
+  },
+  {
+    "name": "Urban Loft",
+    "country": "USA",
+    "price": 300000,
+    "isAvailable": true
+  },
+  {
+    "name": "Beach House",
+    "country": "Australia",
+    "price": 850000,
+    "isAvailable": false
+  },
+  {
+    "name": "City Apartment",
+    "country": "Canada",
+    "price": 500000,
+    "isAvailable": true
+  }
+]
 
-}]
 
 
 module.exports = { propertyData }
